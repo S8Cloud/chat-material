ce038687ecc2438f35522c4e1c7bead3283c6b3b
georgegach <georgeggach@gmail.com>
2016-09-16 13:21:10 +0400

    initial commit

---
+++ public/script.js
@@ -0,0 +1,99 @@
+var App = App || {};
+
+App.socket = io();
+App.nick = "";
+// App.color = Math.floor(200 + Math.random()*55) + "," +
+// 			Math.floor(200 + Math.random()*55) + "," + 
+// 			Math.floor(200 + Math.random()*55);
+App.color = getColor();
+App.uniqueID = null;
+
+App.socket.on('message', function(res)
+{
+	// console.log(res);
+	document.querySelector("#messages").innerHTML += "<li class='chatmessage' style='background-color:hsl(" + res.color + ")'><span class='nick'>"+ res.nick + " </span><span class='msg'>" + res.msg + "</span><span class='time'>" + res.time + "</span></li>";
+	scroll();
+});
+
+App.socket.on('uniqueID', function(id)
+{
+	App.uniqueID = id;
+});
+
+App.socket.on('sysmessage', function(data)
+{
+	// console.log(data);
+	var stbar = document.querySelector(".peopleColors");
+	stbar.innerHTML = "";
+	data.users.forEach(function(item)
+	{
+		if (item != null)
+			stbar.innerHTML += "<span class='coloricle' style='background-color: hsl(" + item.color + ")'><span class='info'>" + item.nick + "</span></span>";
+	})
+
+	var statusmain = document.querySelector(".statusmain")
+	statusmain.innerHTML = "<span class='coloricle'  onclick='changeColor()' style='background-color: hsl(" + App.color + ")'></span>";
+	statusmain.innerHTML += "<div class='nick'>" + App.nick + "</div>";
+
+	if (data.count > 1)
+		statusmain.innerHTML += " <div class='peopleCount'> " + data.count + " people online </div>";
+	else
+		statusmain.innerHTML +=  " <div class='peopleCount'>Only you are online</div>";
+
+});
+
+function scroll() 
+{
+	var msgs = document.querySelector("#messages");
+	msgs.scrollTop = msgs.scrollHeight;
+}
+
+function getColor() 
+{
+	var hue = Math.floor(Math.random() * 120) * 3 ;
+	return hue + ", 60%, 85%";
+};
+
+function changeColor()
+{
+	App.color = getColor();
+	App.socket.emit('updateProfile', 
+		{
+			"nick" : App.nick, 
+			"color" : App.color, 
+			// "uniqueID" : App.uniqueID
+		});
+};
+
+function simulate()
+{
+	var stbar = document.querySelector(".peopleColors");
+	stbar.innerHTML = "";for (var i = 0; i < 10; i++) 
+	{
+		stbar.innerHTML += "<span class='coloricle' onclick='changeColor()' style='background-color: hsl(" + getColor() + ")'><span class='info'>Dude</span></span>";
+	}
+};
+
+function send() 
+{
+	var input = document.querySelector("#m");
+	if (input.value.substr(0,2) == "/c")
+		App.socket.emit('command', input.value.substr(3));
+	else
+		App.socket.emit('message', input.value);
+	input.value = "";
+    return false;
+};
+
+function onConnect() 
+{
+	App.socket.emit('onConnect', {"nick" : App.nick, "color" : App.color});
+}
+
+(function()
+{
+	App.nick = prompt("Name yourself!", "Guest");
+	onConnect();
+	// simulate();
+})();
+	
---
+++ server.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/client.html');
+});
+
+app.use(express.static('public'));
+
+var usercount = 0;
+var uniqueID = -1;
+var users = {
+	private : [],
+	public : []
+};
+
+io.on('connection', function(socket)
+{
+	uniqueID++;
+	var private = {};
+
+	var addedUser = false;
+	// socket.broadcast.emit('sysmessage', users);
+
+	socket.on('message', function(msg)
+	{
+		var user = users.public[private.uniqueID];
+	    io.emit('message', 
+	    	{
+	    		"color" : user.color, 
+	    		"msg" : msg, 
+	    		"time" : new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds(), 
+	    		"nick" : user.nick
+	    	});
+ 	});
+	
+	socket.on('disconnect', function()
+	{
+		if (addedUser)
+			usercount--;
+
+		delete users.public[private.uniqueID];
+		io.sockets.emit('sysmessage', { "users" : users.public, "count" : usercount});
+	});
+
+	socket.on('command', function(req)
+	{
+		console.log(req);
+	});
+
+
+
+	socket.on('updateProfile', function(data)
+	{
+		users.public[private.uniqueID] = data;
+		io.sockets.emit('sysmessage', { "users" : users.public, "count" : usercount});
+	});
+
+
+	socket.on('onConnect', function(data)
+	{
+
+		if (addedUser)
+			return;
+
+		addedUser = true;
+		usercount++;
+		
+		private.uniqueID = uniqueID;
+		users.public[private.uniqueID] = data;
+
+		// io.emit('uniqueID', user.uniqueID); 
+		io.sockets.emit('sysmessage', { "users" : users.public, "count" : usercount});
+	});
+});
+
+http.listen(9999, function(){
+	console.log('listening on *:9999');
+});
+
